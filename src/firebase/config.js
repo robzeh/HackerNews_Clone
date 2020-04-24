8f9f05c5f42effc76977505a628b88f633c6fbd8
Robie Gonzales <robie.gonzaless@gmail.com>
2020-04-24 18:42:06 -0300

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjndHbRFUgE_qnmstnQ6ofH5valg7yluM",
+  authDomain: "hacker-news-clone-6b07d.firebaseapp.com",
+  databaseURL: "https://hacker-news-clone-6b07d.firebaseio.com",
+  projectId: "hacker-news-clone-6b07d",
+  storageBucket: "hacker-news-clone-6b07d.appspot.com",
+  messagingSenderId: "715307669401",
+  appId: "1:715307669401:web:326ce789f2275a1892e508",
+};
+
+export default firebaseConfig;
