18eed118d3d48720fc28658ed14285e5d55cb2ea
Robie Gonzales <robie.gonzaless@gmail.com>
2020-04-26 14:14:06 -0300

    Created Link components and configured to show on news/trending pages

--- src/pages/Tabs/News.js
+++ src/pages/Tabs/News.js
@@ -2,13 +2,15 @@ import React from "react";
 import { IonPage, IonContent } from "@ionic/react";
 import SmallHeader from "../../components/Headers/SmallHeader";
 import LargeHeader from "../../components/Headers/LargeHeader";
+import LinkList from "../../components/Link/LinkList";
 
-const News = () => {
+const News = (props) => {
   return (
     <IonPage>
       <SmallHeader title="News" />
       <IonContent fullscreen>
         <LargeHeader title="News" />
+        <LinkList location={props.location} />
       </IonContent>
     </IonPage>
   );
